17900b60f66686caa998f4cecf251ad492a9ca92
KevinAndresG <3348@holbertonschool.com>
2021-06-18 02:39:23 +0000

    c file to display the size FIX1.1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       printf("Size of char:", sizeof(char) "%zu byte(s)\n");
-	printf("Size of int:", sizeof(int) "%zu byte(s)\n");
-	printf("Size of int:", sizeof(longint) "%zu byte(s)\n");
-	printf("Size of int:", sizeof(longlongint) "%zu byte(s)\n");
-	printf("Size of float:", sizeof(float) "%zu byte(s)\n");
+{       printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int:%d byte(s)\n", sizeof(int));
+	printf("Size of int: %d byte(s)\n", sizeof(long int));
+	printf("Size of int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
