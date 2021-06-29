a41713ba1d2291e9dbf0371e7bd682a74ddc9676
KevinAndresG <3348@holbertonschool.com>
2021-06-29 19:22:04 +0000

    first point FIX

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * reset_to_98: that takes a pointer to an int
- *@*n: a pointer
+ * reset_to_98 - that takes a pointer to an int
+ *@n: a pointer
  *Return: Always 0.
  */
 void reset_to_98(int *n)
