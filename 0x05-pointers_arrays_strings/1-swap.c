1f23964d173412f3969dcb2292abf3efffa5261a
KevinAndresG <3348@holbertonschool.com>
2021-06-29 19:51:16 +0000

    swamp that values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * swap_int - that takes a pointer to an int
+ *@a: a pointer
+ *@b: other pointer
+ *Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int *c, *d, mix;
+	c = a;
+	d = b;
+	mix = *d;
+	*d = *c;
+	*c = mix;
+}
