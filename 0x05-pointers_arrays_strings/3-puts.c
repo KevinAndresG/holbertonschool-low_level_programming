716902afef2b1d6eaa179103e4824d237726dc6a
KevinAndresG <3348@holbertonschool.com>
2021-06-30 00:26:50 +0000

    add the points

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _puts - Write a function that prints a string, followed by a new line
+ *@str: string
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	puts (str);
+}
