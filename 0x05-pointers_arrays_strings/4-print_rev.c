92e46632915dd0c8ad4ed6dcbe4a5d2325e1a926
KevinAndresG <3348@holbertonschool.com>
2021-07-01 18:59:09 +0000

    print a text in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_rev - check the code for Holberton School students.
+ *@s: char
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (j = i; j >= 0; j--)
+	{
+		_putchar (s[j]);
+	}
+	_putchar ('\n');
+}
