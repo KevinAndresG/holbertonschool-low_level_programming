40269ad5e7e2bc5b53e0d59c203e291d0fc007d7
KevinAndresG <3348@holbertonschool.com>
2021-07-05 23:34:08 +0000

    directory 0x07

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);
+char *_strcat(char *dest, char *src);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+
+#endif /* HOLBERTON_H */
