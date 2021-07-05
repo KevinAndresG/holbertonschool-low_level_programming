5a918291e1e4a4b8e9851eaffeaf4691efcbb834
KevinAndresG <3348@holbertonschool.com>
2021-07-05 05:09:22 +0000

    add the 0 point concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strcat : function that concatenates two strings.
+ * @dest: char
+ * @src: char
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+  int c = 0;
+  int x = 0;
+  while (dest[c] != '\0')
+    {
+      c++;
+    }
+  while (src[x] != '\0')
+    {
+      dest[c + x] = src[x];
+	x++;
+    }
+  dest[c + x] = '\0';
+  return (dest);
+}
