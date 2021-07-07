3ac86a999897bb2378a3192a68be9822128df10d
KevinAndresG <3348@holbertonschool.com>
2021-07-07 00:01:34 -0500

    function to return the first segment

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strspn - just accept
+ * @s: char
+ * @accept: char
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int k, j;
+
+unsigned int l = 0;
+
+for (k = 0; s[k] != ','; k++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+if (accept[j] == s[k])
+{
+l++;
+}
+}
+return (l);
+}
