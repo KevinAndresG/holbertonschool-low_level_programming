e6c4ab9c20ff37da11794979cf0db84bce0a3e3e
KevinAndresG <3348@holbertonschool.com>
2021-07-10 01:07:52 -0500

    multipli

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: count the lines argument
+ * @argv: arguments vector
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int x, y, result;
+
+	if (argc == 3)
+	{
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	result = x * y;
+	printf("%d\n", result);
+	}
+	else
+	{
+	printf("Error\n");
+		return (-1);
+	}
+return (0);
+}
