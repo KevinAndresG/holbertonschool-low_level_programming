56b378d2b50f84a8eaffc714df26d0ec45163e53
KevinAndresG <3348@holbertonschool.com>
2021-07-09 21:16:11 -0500

    program that prints the arguments number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: count the lines argument
+ * @argv: arguments vector
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int c;
+	for (c = 0; c < argc; c++)
+	{
+		printf("%d\n", c);
+	}
+	if (argv[c])
+	{
+		return (0);
+	}
+		return (0);
+}
