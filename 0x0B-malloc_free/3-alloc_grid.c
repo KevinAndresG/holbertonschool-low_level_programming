1c2f545103eda0ca01fa55804ca1901b6ee20213
KevinAndresG <3348@holbertonschool.com>
2021-07-14 20:46:12 -0500

    print a grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,24 +10,25 @@
 int **alloc_grid(int width, int height)
 {
 	int **array = NULL;
-	int j = 0, k = 0;
+	int j = 0;
+	int k = 0;
 
-	array = malloc(height * sizeof(int*));
+	array = malloc(height * sizeof(int *));
 	if (array == NULL)
 	{
 		return (NULL);
 	}
 	for (j = 0; j < height; j++)
 	{
-	array[j] = malloc(width * sizeof(int*));
+	array[j] = malloc(width * sizeof(int *));
 		if (array[j] == NULL)
 		{
-			return(NULL);
+			return (NULL);
 		}
-		for (k = 0; k < width; k++)
-			{
-				array[j][k] = 0;
-			}
+	for (k = 0; k < width; k++)
+	{
+		array[j][k] = 0;
+	}
 	}
 	return (array);
 }
