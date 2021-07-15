46562ce5582516e01d6004dc6346a0dabf5521dd
KevinAndresG <3348@holbertonschool.com>
2021-07-15 00:55:04 -0500

    no kenia

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,3 +1,4 @@
+
 #include "holberton.h"
 #include <stdlib.h>
 
@@ -9,33 +10,37 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **array = NULL;
-	int j = 0;
-	int k = 0;
+    int **array = NULL;
+    int j = 0;
+    int k = 0;
 
-	if (width <= 0 && height <= 0)
-	{
-		return (NULL);
-	}
-	array = malloc(height * sizeof(int *));
-	if (array == NULL)
-	{
-		free(array);
-		return (NULL);
-	}
+    if (width <= 0 || height <= 0)
+    {
+        return (NULL);
+    }
+    array = malloc(height * sizeof(int *));
+    if (array == NULL)
+    {
+        return (NULL);
+    }
 
-	for (j = 0; j < height; j++)
-	{
-	array[j] = malloc(width * sizeof(int));
-		if (array[j] == NULL)
-		{
-			free(array[j]);
-			return (NULL);
-		}
-		for (k = 0; k < width; k++)
-		{
-			array[j][k] = 0;
-		}
-	}
-	return (array);
+    for (j = 0; j < height; j++)
+    {
+        array[j] = malloc(width * sizeof(int));
+
+        for (k = 0; k < width; k++)
+        {
+            if (array[j] == NULL)
+            {
+                for (; j >= 0; j--)
+                {
+                    free(array[j]);
+                }
+                free(array);
+                return (NULL);
+            }
+            array[j][k] = 0;
+        }
+    }
+    return (array);
 }
