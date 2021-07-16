9f7429a1de123ca999991caf3b41751de6b91085
KevinAndresG <3348@holbertonschool.com>
2021-07-16 12:55:42 -0500

    fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,20 +8,18 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	char *k = NULL;
-	unsigned int j;
+
+	unsigned int *j;
+
 	if (b == 0)
 	{
 		return (NULL);
 	}
-	k = malloc(b * sizeof(int));
-	if (k == 0)
+	j = malloc(b * sizeof(int));
+	if (j == 0)
 	{
 		return (NULL);
 	}
-	for (j = 0; j <= b; j++)
-	{
-		k[j] = b;
-	}
-	return (k);
+
+	return (j);
 }
