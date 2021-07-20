2c98c0efb51684555c5b1cdc98ec2368d693d66f
KevinAndresG <3348@holbertonschool.com>
2021-07-19 23:12:57 -0500

    the fisrt structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - some members in the dog struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: the dog travels
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /*DOG_H*/
