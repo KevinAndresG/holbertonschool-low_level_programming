158f64aed68a12ed4ca635c1aa2f08e690738236
KevinAndresG <3348@holbertonschool.com>
2021-07-21 23:33:30 -0500

    print numbers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,14 +17,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	}
 	if (array != 0 && cmp != 0)
 	{
-	for (k = 0; k < size; k++)
-	{
-		res = cmp(array[k]);
-		if (res == 1)
+		for (k = 0; k < size; k++)
 		{
-			return (k);
+			res = cmp(array[k]);
+			if (res == 1)
+			{
+				return (k);
+			}
 		}
-	}
 	}
 	return (-1);
 }
