cbf6a2c04c412f1633a2e02c33ec038485df95a0
KevinAndresG <3348@holbertonschool.com>
2021-07-21 23:32:47 -0500

    print numbers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function that executes a function given as a parameter
+ * @array: a int pointer
+ * @size: var int type
+ * @cmp: the function that call another function
+ * Return: return an int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int k, res;
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array != 0 && cmp != 0)
+	{
+	for (k = 0; k < size; k++)
+	{
+		res = cmp(array[k]);
+		if (res == 1)
+		{
+			return (k);
+		}
+	}
+	}
+	return (-1);
+}
