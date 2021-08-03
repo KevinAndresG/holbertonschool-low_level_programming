4753ecffa6b79bdb59707d338a36249587f90e6e
KevinAndresG <3348@holbertonschool.com>
2021-08-02 23:48:22 -0500

    add new node at the beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,5 +9,24 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	int r = 0;
+	list_t *first = malloc(sizeof(list_t));
 
+	if (first == NULL)
+	{
+		return (NULL);
+	}
+	while (str[r] != '\0')
+	{
+		r++;
+	}
+	first->str = strdup(str);
+	first->len = r;
+	if (head != NULL)
+	{
+		first->next = (*head);
+	}
+	(*head) = first;
+
+	return (first);
 }
