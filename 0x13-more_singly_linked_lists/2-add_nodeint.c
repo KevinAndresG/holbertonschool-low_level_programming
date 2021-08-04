9fe15a26f5dae3ca9d2fb0127a5451d751b9686b
KevinAndresG <3348@holbertonschool.com>
2021-08-03 21:13:05 -0500

    fixing add new node at the beggining

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,9 +8,13 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *new = malloc(sizeof(listint_t));
-new->n = n;
-new->next = *head;
-*head = new;
-return (new);
+	listint_t *new = malloc(sizeof(listint_t));
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	return (new);
 }
