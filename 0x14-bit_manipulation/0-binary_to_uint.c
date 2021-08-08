3cd8c133634ed9c46c862d87b893a13841994c5c
KevinAndresG <3348@holbertonschool.com>
2021-08-08 17:44:27 -0500

    passing binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,5 +8,29 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int n = 0;
+	int j = 0;
 
+	if (b == NULL)
+	{
+		return (1);
+	}
+
+	for (j = 0; b[j] != '\0'; j++)
+	{
+		if (b[j] != 48 && b[j] != 49)
+		{
+			return (0);
+		}
+	}
+
+	for (j = 0; b[j] != '\0'; j++)
+	{
+		n <<= 1;
+		if (b[j] == 49)
+		{
+			n += 1;
+		}
+	}
+	return (n);
 }
