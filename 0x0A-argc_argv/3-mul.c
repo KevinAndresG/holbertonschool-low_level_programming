4894bf83af42c3eab6c1248f6dd29a3130ad7eae
KevinAndresG <3348@holbertonschool.com>
2021-08-11 10:19:53 -0500

    fixing that point addin else

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,17 @@ int main(int argc, char *argv[])
 {
 	int x, y, result;
 
-	if (argc == 4)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (-1);
 	}
-
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	result = x * y;
-	printf("%d", result);
-
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		result = x * y;
+		printf("%d\n", result);
+	}
 	return (0);
 }
