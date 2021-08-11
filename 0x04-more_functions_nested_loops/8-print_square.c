73bdee1f121c6673fa361c634a80a14e03862f3f
KevinAndresG <3348@holbertonschool.com>
2021-08-11 12:38:52 -0500

    fixiong betty point 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_square - function that prints a square
+ * @size: an int
+ * Return: nothing
+ */
 void print_square(int size)
 {
 	int k = 0;
