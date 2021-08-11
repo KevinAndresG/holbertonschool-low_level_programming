9bc4a688ffb82049d3e0af9a194a2d6157656ce7
KevinAndresG <3348@holbertonschool.com>
2021-08-11 01:46:47 -0500

    creating file with FILE I|O fix 2.0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int i = 0, wr = 0;
 	int fd = 0;
-	char *buffer = NULL;
+	/*char *buffer = NULL;*/
 
 	if (!filename)
 	{
@@ -23,22 +23,25 @@ int create_file(const char *filename, char *text_content)
 	for (; text_content[i] != '\0'; i++)
 	{
 	}
-	buffer = malloc(sizeof(char) * i);
-	if (buffer == NULL)
-	{
-		return (0);
-	}
+	/**
+	* buffer = malloc(sizeof(char) * i);
+	* if (buffer == NULL)
+	* {
+	* return (0);
+	* }
+	*/
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	wr = write(fd, buffer, i);
+	wr = write(fd, text_content, i);
+	/*write(fd, "\n", 1)*/
 	if (wr == -1)
 	{
 		return (-1);
 	}
-	free(buffer);
+	/*free(buffer);*/
 	close(fd);
 	return (1);
 }
