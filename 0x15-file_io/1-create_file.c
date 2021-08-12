aba0b22509ec76aabd452ae213b1677e889bb8f1
KevinAndresG <3348@holbertonschool.com>
2021-08-11 21:55:29 -0500

    creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,38 +10,31 @@ int create_file(const char *filename, char *text_content)
 {
 	int i = 0, wr = 0;
 	int fd = 0;
-	/*char *buffer = NULL;*/
 
 	if (!filename)
 	{
 		return (-1);
 	}
-	if (!text_content)
-	{
-		return (-1);
-	}
-	for (; text_content[i] != '\0'; i++)
-	{
-	}
-	/**
-	* buffer = malloc(sizeof(char) * i);
-	* if (buffer == NULL)
-	* {
-	* return (0);
-	* }
-	*/
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	for (; text_content[i] != '\0'; i++)
+	{
+	}
+
 	wr = write(fd, text_content, i);
 	/*write(fd, "\n", 1)*/
 	if (wr == -1)
 	{
 		return (-1);
 	}
-	/*free(buffer);*/
 	close(fd);
 	return (1);
 }
