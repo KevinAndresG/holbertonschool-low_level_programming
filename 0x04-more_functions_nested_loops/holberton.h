b46cb992d109418ee7813eec1e193f79f9707944
KevinAndresG <3348@holbertonschool.com>
2021-08-21 22:20:28 -0500

    is upper

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,13 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int _putchar(char c);
 int _isupper(int c);
- int _isdigit(int c);
- int mul(int a, int b);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_square(int size);
 
-#endif /* MAIN_H */
+#endif /* HOLBERTON_H */
