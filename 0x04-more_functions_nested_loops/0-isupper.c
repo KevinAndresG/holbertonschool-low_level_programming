3cd2496e260b7638dba553089c9bacaec8272974
KevinAndresG <3348@holbertonschool.com>
2021-08-21 22:10:46 -0500

    function to check upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: an int
+ * Return: an int
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
+int _isupper(int c);
+ int _isdigit(int c);
+ int mul(int a, int b);
 void print_square(int size);
 
 #endif /* MAIN_H */
