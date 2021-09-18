e6c10cf00c013eec25ef902b15283520fcf1e09d
KevinAndresG <3348@holbertonschool.com>
2021-09-18 17:04:58 -0500

    sum the nodes

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 /**
  * sum_dlistint - function that returns the sum of all the data
- * @head:
+ * @head: the list
  * Return: an int
  */
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
+
 	while (head != NULL)
 	{
 		sum = sum + head->n;
