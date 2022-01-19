16a9daacc31771e6124a57638f9ec91da1dc5b82
KevinAndresG <3348@holbertonschool.com>
2022-01-19 17:20:00 -0500

    trying to fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -18,7 +18,6 @@ int binary_search(int *array, size_t size, int value)
 	{
 		return (-1);
 	}
-
 	while (l <= r)
 	{
 		size_t i = 0;
@@ -35,14 +34,12 @@ int binary_search(int *array, size_t size, int value)
 				printf("%d, ", array[i]);
 		}
 		mid = (l + r) / 2;
-
 		if (array[mid] == value)
 			return (mid);
-		else if (array[mid] < value)
-			l = mid + 1;
-
-		else
+		else if (array[mid] > value)
 			r = mid - 1;
+		else
+			l = mid + 1;
 	}
 	return (-1);
 }
